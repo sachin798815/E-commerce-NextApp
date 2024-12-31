04aa28d68f913885aaab3d7ea77391b3a9855eb8
sachin798815 <sachin798815@gmail.com>
2024-12-31 22:36:08 +0530

    base nav

--- src/Components/Header/NavbarComponent.js
+++ src/Components/Header/NavbarComponent.js
@@ -1,9 +1,42 @@
-const NavbarComponent=()=>{
+import styles from './NavbarComponent.module.css';
 
-    return(
-        <>
-        <nav></nav>
-        </>
-    )
-}
+const NavbarComponent = () => {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.logo}>
+        <Link href="/">
+          <a>TechStore</a>
+        </Link>
+      </div>
+      <ul className={styles.navLinks}>
+        <li>
+          <Link href="/categories">
+            <a>Categories</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/deals">
+            <a>Deals</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/contact">
+            <a>Contact</a>
+          </Link>
+        </li>
+      </ul>
+      <div className={styles.cart}>
+        <Link href="/cart">
+          <a>Cart</a>
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
 export default NavbarComponent;
