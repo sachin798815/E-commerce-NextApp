f573eb37a8f4a250f405fad4102c874ed3d7e345
sachin798815 <sachin798815@gmail.com>
2025-03-31 12:04:55 +0530

    demo categories added for store

--- src/Components/Body/StorePageComponent.js
+++ src/Components/Body/StorePageComponent.js
@@ -1,3 +1,17 @@
+const categories = [
+  {
+    title: 'Phones',
+    items: ['iPhone 14', 'Samsung Galaxy S23', 'OnePlus 11', 'Google Pixel 8'],
+    img: '/images/phones.jpg'
+  },
+  {
+    title: 'Earphones',
+    items: ['AirPods Pro', 'Sony WF-1000XM4', 'Bose QuietComfort Earbuds', 'JBL Tune 230NC'],
+    img: '/images/earphones.jpg'
+  }
+];
+
+
 const StorePageComponent = () => {
   return (
     <div>
