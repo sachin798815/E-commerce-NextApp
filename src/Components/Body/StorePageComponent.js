8d337c0ccdff1fb7656dbbb6feea14a1db2b37b1
sachin798815 <sachin798815@gmail.com>
2025-04-04 13:13:49 +0530

    storepage carousel added

--- src/Components/Body/StorePageComponent.js
+++ src/Components/Body/StorePageComponent.js
@@ -1,35 +1,20 @@
 import styles from './StorePageComponent.module.css';
-
+import CategoryCarousel from './CategoryCarousel';
 
 const categories = [
-  {
-    title: 'Phones',
-    items: ['iPhone 14', 'Samsung Galaxy S23', 'OnePlus 11', 'Google Pixel 8'],
-    img: '/images/phones.jpg'
-  },
-  {
-    title: 'Earphones',
-    items: ['AirPods Pro', 'Sony WF-1000XM4', 'Bose QuietComfort Earbuds', 'JBL Tune 230NC'],
-    img: '/images/earphones.jpg'
-  }
+  { title: 'Phones', items: ['iPhone 14', 'Samsung Galaxy S23'], img: '/images/phones.jpg' },
+  { title: 'Earphones', items: ['AirPods Pro', 'Sony WF-1000XM4'], img: '/images/earphones.jpg' }
 ];
-
 
 const StorePageComponent = () => {
   return (
-    <div>
+    <div className={styles.storePage}>
       <h1>Shop by Categories</h1>
       {categories.map((category, index) => (
-        <CategoryCarousel 
-          key={index} 
-          title={category.title} 
-          items={category.items} 
-          img={category.img} 
-        />
+        <CategoryCarousel key={index} title={category.title} items={category.items} img={category.img} />
       ))}
     </div>
   );
 };
-
 
 export default StorePageComponent;
