052f071eb76cdd59f9edb2e868b41008ace958dd
sachin798815 <sachin798815@gmail.com>
2025-04-08 11:48:09 +0530

    carousel styles fixed

--- src/Components/Body/CategoryCarouselComponent.js
+++ src/Components/Body/CategoryCarouselComponent.js
@@ -3,12 +3,12 @@ import styles from './CategoryCarousel.module.css';
 const CategoryCarousel = ({ title, items, img }) => {
   return (
     <div className={styles.carouselContainer}>
-      <h2>{title}</h2>
+      <h2 className={styles.carouselTitle}>{title}</h2>
       <div className={styles.carousel}>
         {items.map((item, index) => (
           <div key={index} className={styles.card}>
             <img src={img} alt={item} className={styles.productImage} />
-            <p>{item}</p>
+            <p className={styles.productName}>{item}</p>
           </div>
         ))}
       </div>
